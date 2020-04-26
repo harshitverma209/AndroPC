6a7d74da88e1b1ef2fcaf2e7662d8a71a1ef9f9a
Awesome <harshitverma209@gmail.com>
2020-04-26 19:44:49 +0530

    / ‘app/src/main/java/com/tba/andropc/models/DeviceModel.java’

--- app/src/main/java/com/tba/andropc/models/DeviceModel.java
+++ app/src/main/java/com/tba/andropc/models/DeviceModel.java
@@ -1,6 +1,23 @@
 package com.tba.andropc.models;
 
 public class DeviceModel {
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
     String deviceName;
+
+    public String getDeviceMac() {
+        return deviceMac;
+    }
+
+    public void setDeviceMac(String deviceMac) {
+        this.deviceMac = deviceMac;
+    }
+
     String deviceMac;
 }
