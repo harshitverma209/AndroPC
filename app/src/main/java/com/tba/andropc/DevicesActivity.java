3450dddcfb953f67b44a067d46fc125304ab3c12
Awesome <harshitverma209@gmail.com>
2020-04-26 16:05:20 +0530

    / ‘app/src/main/AndroidManifest.xml’ + ‘app/src/main/java/com/tba/andropc/DevicesActivity.java’ + ‘app/src/main/res/layout/activity_devices.xml’

---
+++ app/src/main/java/com/tba/andropc/DevicesActivity.java
@@ -0,0 +1,13 @@
+package com.tba.andropc;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DevicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devices);
+    }
+}
