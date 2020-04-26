93680b94ee44ef7bb86c5a0937032527c868d74d
hulk <sallyabhi727@gmail.com>
2020-04-26 18:41:33 +0530

    / ‘app/src/main/java/com/tba/andropc/deviceAdapter.kt’ / ‘app/src/main/res/layout/activity_devices.xml’ / ‘app/src/main/res/layout/activity_main.xml’ / ‘app/src/main/res/layout/devicename.xml’

--- app/src/main/java/com/tba/andropc/deviceAdapter.kt
+++ app/src/main/java/com/tba/andropc/deviceAdapter.kt
@@ -1,4 +1,18 @@
 package com.tba.andropc
 
-class deviceAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class deviceAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
