522353b7a3ef4454d53dad457bd2be6f85537726
Awesome <harshitverma209@gmail.com>
2020-04-26 21:43:55 +0530

    / ‘app/src/main/java/com/tba/andropc/DeviceAdapter.kt’ / ‘app/src/main/java/com/tba/andropc/DevicesActivity.java’

--- app/src/main/java/com/tba/andropc/DevicesActivity.java
+++ app/src/main/java/com/tba/andropc/DevicesActivity.java
@@ -35,7 +35,7 @@ public class DevicesActivity extends AppCompatActivity {
 
         deviceRecyclerView=findViewById(R.id.rec);
 
-        dev= new DeviceAdapter(this);
+        dev= new DeviceAdapter(this, );
         deviceRecyclerView.setAdapter(dev);
 
         LinearLayoutManager lm = new LinearLayoutManager(this);
