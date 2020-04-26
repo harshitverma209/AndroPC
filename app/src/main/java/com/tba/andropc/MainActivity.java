8cf84b718050655134d5c04c7ddeb0c63cf111ed
hulk <sallyabhi727@gmail.com>
2020-04-26 16:11:54 +0530

    / ‘app/src/main/java/com/tba/andropc/MainActivity.java’ / ‘app/src/main/res/layout/activity_main.xml’

--- app/src/main/java/com/tba/andropc/MainActivity.java
+++ app/src/main/java/com/tba/andropc/MainActivity.java
@@ -3,13 +3,31 @@ package com.tba.andropc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText etMain;
+Button btnMain;
+String connect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etMain=findViewById(R.id.etMaibn);
+        btnMain=findViewById(R.id.btnMain);
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            connect=etMain.getText().toString();
+            }
+        });
 
+
+    }
+    public void sendTextToPC()
+    {
+        
     }
 }
