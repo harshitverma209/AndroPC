29763b40611c7a18a8a90224fad9321da631418f
Awesome <harshitverma209@gmail.com>
2020-04-26 21:13:00 +0530

    / ‘app/src/main/java/com/tba/andropc/DevicesActivity.java’

--- app/src/main/java/com/tba/andropc/DevicesActivity.java
+++ app/src/main/java/com/tba/andropc/DevicesActivity.java
@@ -106,7 +106,7 @@ public class DevicesActivity extends AppCompatActivity {
     };
 
     private void update() {
-        notifyAll();
+        not
     }
 
     private void showToast(String s) {
