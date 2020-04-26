57bd35f4e9bae372d5f20859fd5822ecae711bd6
Awesome <harshitverma209@gmail.com>
2020-04-26 17:08:00 +0530

    / ‘app/src/main/java/com/tba/andropc/DevicesActivity.java’

--- app/src/main/java/com/tba/andropc/DevicesActivity.java
+++ app/src/main/java/com/tba/andropc/DevicesActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.UUID;
 
@@ -37,4 +42,25 @@ public class DevicesActivity extends AppCompatActivity {
             mBluetoothAdapter.disable();
         }
     }
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                Log.d("chech","discovering");
+
+                //discovery starts, we can show progress dialog or perform other tasks
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                //discovery finishes, dismis progress dialog
+
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //bluetooth device found
+                Log.d("chech","got it");
+                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+//                s.add(device.getName()+"\n"+device.getAddress());
+                //showToast("Found device " + device.getName());
+            }
+        }
+    };
 }
