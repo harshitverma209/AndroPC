41a1c1f56e08903443e07b1fee2f115c478685e6
Awesome <harshitverma209@gmail.com>
2020-04-26 17:01:45 +0530

    / ‘app/src/main/java/com/tba/andropc/DevicesActivity.java’

--- app/src/main/java/com/tba/andropc/DevicesActivity.java
+++ app/src/main/java/com/tba/andropc/DevicesActivity.java
@@ -17,6 +17,7 @@ public class DevicesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devices);
         BA=BluetoothAdapter.getDefaultAdapter();
+        uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 //        deviceList=new RecyclerView(this,);
     }
 }
