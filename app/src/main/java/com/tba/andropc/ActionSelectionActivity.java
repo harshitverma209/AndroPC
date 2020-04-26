a12704fb56ed97e06445d2dbcfd0469076b0702b
Awesome <harshitverma209@gmail.com>
2020-04-26 18:14:29 +0530

    / ‘app/src/main/java/com/tba/andropc/ActionSelectionActivity.java’ / ‘app/src/main/res/layout/activity_action_selection.xml’

--- app/src/main/java/com/tba/andropc/ActionSelectionActivity.java
+++ app/src/main/java/com/tba/andropc/ActionSelectionActivity.java
@@ -3,12 +3,14 @@ package com.tba.andropc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ActionSelectionActivity extends AppCompatActivity {
-
+    Button act_trackpad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_selection);
+        act_trackpad=findViewById()
     }
 }
