d2942a736f0797a40bdcb83623c6e71d0e091634
hulk <sallyabhi727@gmail.com>
2020-04-27 17:38:48 +0530

    / ‘.idea/misc.xml’ / ‘app/src/main/AndroidManifest.xml’ + ‘app/src/main/java/com/tba/andropc/TrackPad.java’ + ‘app/src/main/res/layout/activity_track_pad.xml’

---
+++ app/src/main/java/com/tba/andropc/TrackPad.java
@@ -0,0 +1,14 @@
+package com.tba.andropc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrackPad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_pad);
+    }
+}
