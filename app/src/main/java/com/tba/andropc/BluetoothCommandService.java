930c9abe6d5be9d62ab7415b150f99e120741673
Awesome <harshitverma209@gmail.com>
2020-04-27 14:07:32 +0530

    / ‘app/src/main/java/com/tba/andropc/BluetoothCommandService.java’

--- app/src/main/java/com/tba/andropc/BluetoothCommandService.java
+++ app/src/main/java/com/tba/andropc/BluetoothCommandService.java
@@ -4,9 +4,15 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import java.util.UUID;
+
 public class BluetoothCommandService extends Service {
+
+    private static final UUID MY_UUID = UUID.fromString("04c6093b-0000-1000-8000-00805f9b34fb");
+
+
     public BluetoothCommandService() {
-        
+
     }
 
     @Override
