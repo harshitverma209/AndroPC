1b69c98648713cded81dcad93f3ee564cc467f15
Awesome <harshitverma209@gmail.com>
2020-04-27 14:14:37 +0530

    / ‘app/src/main/java/com/tba/andropc/ActionSelectionActivity.java’

--- app/src/main/java/com/tba/andropc/ActionSelectionActivity.java
+++ app/src/main/java/com/tba/andropc/ActionSelectionActivity.java
@@ -8,7 +8,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class ActionSelectionActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+
+
     Button act_trackpad;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
