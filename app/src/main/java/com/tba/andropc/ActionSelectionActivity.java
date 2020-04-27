7e72df3c2c05fbdfc5d3b88c0901a6ae78fd5216
Awesome <harshitverma209@gmail.com>
2020-04-27 14:15:24 +0530

    / ‘app/src/main/java/com/tba/andropc/ActionSelectionActivity.java’

--- app/src/main/java/com/tba/andropc/ActionSelectionActivity.java
+++ app/src/main/java/com/tba/andropc/ActionSelectionActivity.java
@@ -2,6 +2,7 @@ package com.tba.andropc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,23 @@ public class ActionSelectionActivity extends AppCompatActivity {
 
 
     private static final int REQUEST_CONNECT_DEVICE = 1;
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothCommandService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
+    // Local Bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
+    // Member object for Bluetooth Command Service
+    private BluetoothCommandService mCommandService = null;
 
 
     Button act_trackpad;
