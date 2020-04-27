bdc60b1c5b13ed8628ac3da051e00976e6e520a2
Awesome <harshitverma209@gmail.com>
2020-04-27 16:40:07 +0530

    / ‘app/src/main/java/com/tba/andropc/ActionSelectionActivity.java’

--- app/src/main/java/com/tba/andropc/ActionSelectionActivity.java
+++ app/src/main/java/com/tba/andropc/ActionSelectionActivity.java
@@ -56,7 +56,7 @@ public class ActionSelectionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(ActionSelectionActivity.this, MainActivity.class);
-                intent.putExtra
+                
                 startActivity(intent);
             }
         });
