0c855ef0f8ec90125b3f1f54fdc829f02ef2028d
Awesome <harshitverma209@gmail.com>
2020-04-27 17:14:36 +0530

    / ‘app/src/main/java/com/tba/andropc/ActionSelectionActivity.java’

--- app/src/main/java/com/tba/andropc/ActionSelectionActivity.java
+++ app/src/main/java/com/tba/andropc/ActionSelectionActivity.java
@@ -16,20 +16,8 @@ import android.widget.Toast;
 public class ActionSelectionActivity extends AppCompatActivity {
 
 
-    private static final int REQUEST_CONNECT_DEVICE = 1;
-    // Message types sent from the BluetoothChatService Handler
-    public static final int MESSAGE_STATE_CHANGE = 1;
-    public static final int MESSAGE_READ = 2;
-    public static final int MESSAGE_WRITE = 3;
-    public static final int MESSAGE_DEVICE_NAME = 4;
-    public static final int MESSAGE_TOAST = 5;
 
-    // Key names received from the BluetoothCommandService Handler
-    public static final String DEVICE_NAME = "device_name";
-    public static final String TOAST = "toast";
 
-    // Name of the connected device
-    private String mConnectedDeviceName = null;
     // Local Bluetooth adapter
     private BluetoothAdapter mBluetoothAdapter = null;
     // Member object for Bluetooth Command Service
