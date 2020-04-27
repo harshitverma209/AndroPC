46acb6904db12511df9ae7674ee768161aec1bb3
Awesome <harshitverma209@gmail.com>
2020-04-27 15:36:58 +0530

    / ‘app/src/main/java/com/tba/andropc/DeviceAdapter.kt’

--- app/src/main/java/com/tba/andropc/DeviceAdapter.kt
+++ app/src/main/java/com/tba/andropc/DeviceAdapter.kt
@@ -37,7 +37,7 @@ class DeviceAdapter(context: Context, deviceModel: ArrayList<DeviceModel>): Recy
             itemView.name.text= dev.deviceName;
             itemView.address.text=dev.deviceMac;
             itemView.setOnClickListener {
-                val intent = Intent(,ActionSelectionActivity::class.java)
+                val intent = Intent(this,ActionSelectionActivity::class.java)
                 startActivity(intent)
             }
         }
