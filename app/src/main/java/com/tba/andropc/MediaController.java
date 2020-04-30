b19e089a3133edcfe2cd7b7c4606358b71b855f6
Awesome <harshitverma209@gmail.com>
2020-04-30 17:03:59 +0530

    / ‘app/src/main/java/com/tba/andropc/MediaController.java’ / ‘app/src/main/res/layout-v21/activity_media_controller.xml’

--- app/src/main/java/com/tba/andropc/MediaController.java
+++ app/src/main/java/com/tba/andropc/MediaController.java
@@ -7,8 +7,9 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class MediaController extends AppCompatActivity {
-    final String PLAY="1",REWIND="2",FORWARD="3",VOLUMEUP="4",VOLUMEDOWN="5",SILENT="6",FULLSCREEN="7";
+    final String PLAY="1",REWIND="2",FORWARD="3",VOLUMEUP="4",VOLUMEDOWN="5",MUTE="6",FULLSCREEN="7";
     ImageButton play,rewind,forward,volumeup,volumedown,mute,fullscreen;
+    View.OnClickListener mediaControlsListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,47 +28,65 @@ public class MediaController extends AppCompatActivity {
         volumeup.setImageResource(R.drawable.volumeupmedia);
         mute.setImageResource(R.drawable.mute);
         fullscreen.setImageResource(R.drawable.fullscreenmedia);
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BluetoothCommandService.write(PLAY);
-            }
-        });
-        rewind.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BluetoothCommandService.write(REWIND);
-            }
-        });
-        forward.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BluetoothCommandService.write(FORWARD);
-            }
-        });
-        fullscreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BluetoothCommandService.write(FULLSCREEN);
-            }
-        });
-        volumeup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BluetoothCommandService.write(VOLUMEUP);
-            }
-        });
-        volumedown.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BluetoothCommandService.write(VOLUMEDOWN);
-            }
-        });
-        mute.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BluetoothCommandService.write(SILENT);
-            }
-        });
+        initMedidaControlsListener();
+        play.setOnClickListener(mediaControlsListener);
+        rewind.setOnClickListener(mediaControlsListener);
+        forward.setOnClickListener(mediaControlsListener);
+        volumeup.setOnClickListener(mediaControlsListener);
+        volumedown.setOnClickListener(mediaControlsListener);
+        fullscreen.setOnClickListener(mediaControlsListener);
+        mute.setOnClickListener(mediaControlsListener);
+
     }
+
+    private void initMedidaControlsListener() {
+        mediaControlsListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(v.getId()){
+                    case R.id.play:
+                    {
+                        BluetoothCommandService.write(PLAY);
+                        break;
+                    }
+                    case R.id.volumeup:
+                    {
+                        BluetoothCommandService.write(VOLUMEUP);
+                        break;
+                    }
+                    case R.id.volumedown:
+                    {
+                        BluetoothCommandService.write(VOLUMEDOWN);
+                        break;
+                    }
+                    case R.id.fullscreen:
+                    {
+                        BluetoothCommandService.write(FULLSCREEN);
+                        break;
+                    }
+                    case R.id.forward:
+                    {
+                        BluetoothCommandService.write(FORWARD);
+                        break;
+                    }
+                    case R.id.rewind:
+                    {
+                        BluetoothCommandService.write(REWIND);
+                        break;
+                    }
+                    case R.id.mute:
+                    {
+                        BluetoothCommandService.write(MUTE);
+                        break;
+                    }
+                    default:
+                    {
+
+                    }
+                }
+
+            }
+        };
+    }
+
 }
