67dfc11b659b91b8c0d0334e62218c0330baf329
hulk <sallyabhi727@gmail.com>
2020-05-01 13:31:08 +0530

    / ‘.idea/misc.xml’ / ‘app/src/main/java/com/tba/andropc/DeviceAdapter.kt’

--- app/src/main/java/com/tba/andropc/DeviceAdapter.kt
+++ app/src/main/java/com/tba/andropc/DeviceAdapter.kt
@@ -1,13 +1,12 @@
 package com.tba.andropc
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.tba.andropc.models.DeviceModel
 import kotlinx.android.synthetic.main.devicename.view.*
 import java.util.*
 
